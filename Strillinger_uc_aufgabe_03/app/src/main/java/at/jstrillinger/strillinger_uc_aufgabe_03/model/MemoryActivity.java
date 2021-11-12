dab4c30f763b89eb2b99a56feb11f64916c24dd6
Josef Strillinger <jstrillinger@tsn.at>
2021-11-12 20:02:45 +0100

    1

---
+++ Strillinger_uc_aufgabe_03/app/src/main/java/at/jstrillinger/strillinger_uc_aufgabe_03/model/MemoryActivity.java
@@ -0,0 +1,5 @@
+package at.jstrillinger.strillinger_uc_aufgabe_03.model;
+
+public class MemoryActivity {
+
+}
--- Strillinger_uc_aufgabe_03/app/src/main/java/at/jstrillinger/strillinger_uc_aufgabe_03/model/Playground.java
+++ Strillinger_uc_aufgabe_03/app/src/main/java/at/jstrillinger/strillinger_uc_aufgabe_03/model/Playground.java
@@ -1,6 +1,9 @@
 package at.jstrillinger.strillinger_uc_aufgabe_03.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Playground {
 
@@ -13,10 +16,34 @@ public class Playground {
     }
 
     public void init(){
+        //values für die verschiedenen Karten zuweisen, dabei ist zu beachten, das Pärchen immer die selben values haben
+        //Idee zum Vorgehen: values in eine ArrayList/ ein Array geben, dann mit Random werte generieren lassen.
+        //Zu beachten: Man braucht nur halb so viele Werte, wie man Karten hat, weil es ja Pärchen sind
+        //Lösungswerg: Überprüfen, ob das bestimmte value bereits existiert
 
-        List<Card> cards;
+        Random rand = new Random();
+        ArrayList<Integer> helpVal = new ArrayList<Integer>();
+        for(int i = 1; i < getNrPairs(); i++){
+            int help = rand.nextInt(getNrPairs()/2);
+            if(!helpVal.contains(help)){
+                helpVal.add(help);
+            }
+        }
+
+
+        //Position: Herausfinden, wie man Position setzt und wie man prüft, ob dort schon was liegt
+        //Lösung: vermutlich die equals von Position
+
+
+
+        
         
 
+    }
+
+    private int getNrPairs(){
+        int numAnz = cards.length/2;
+        return numAnz;
     }
 
 }
